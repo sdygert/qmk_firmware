b685ece9fc1f835a8ad012d7d2cfe4e0885ba280
sdygert <sethendrissdygert@gmail.com>
2022-06-11 13:44:28 -0700

    Add Space Cadet

--- keyboards/ergodox_ez/keymaps/sdygert/keymap.c
+++ keyboards/ergodox_ez/keymaps/sdygert/keymap.c
@@ -23,7 +23,7 @@ enum custom_keycodes {
 // LT(SYMB,KC_GRV) doesn't make much sense since there's two behaviors for holding down keys
 // Mod Tap with ORing
 // Mouse Delay andd other mouse settings in config.h
-// Space Cadet
+// Replace Key Lock (made obsolete by caps word and space cadet)
 
 // Potential Future Upgrades:
 // Leader Keys
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | BkSp   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
  * |--------+------+------+------+------+------|CtlAlt|           | Lock |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
+ * |LShfLPrn|Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl|RShfRPrn|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |Grv/L1|  '"  |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_EQL,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_LEFT,              KC_RGHT,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,              KC_MINS,
   KC_DEL,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    TG(SYMB),             TG(MDIA),     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,              KC_BSLS,
   KC_BSPC,         KC_A,        KC_S,          KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), GUI_T(KC_QUOT),
-  KC_LSFT,         CTL_T(KC_Z), KC_X,          KC_C,    KC_V,    KC_B,    LCA(KC_NO),           KC_LOCK,      KC_N,    KC_M,    KC_COMM, KC_DOT,  CTL_T(KC_SLSH),    KC_RSFT,
+  KC_LSPO,         CTL_T(KC_Z), KC_X,          KC_C,    KC_V,    KC_B,    LCA(KC_NO),           KC_LOCK,      KC_N,    KC_M,    KC_COMM, KC_DOT,  CTL_T(KC_SLSH),    KC_RSPC,
   LT(SYMB,KC_GRV), KC_QUOT,     LALT(KC_LSFT), KC_LEFT, KC_RGHT,                                                       KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC,           TT(SYMB),
                                                            ALT_T(KC_APP), KC_LGUI,              KC_LALT, CTL_T(KC_ESC),
                                                                           KC_HOME,              KC_PGUP,
