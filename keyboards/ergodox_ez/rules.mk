7ca041212f77d601da294f0eb6e89aeecbf77bd0
sdygert <sethendrissdygert@gmail.com>
2022-06-13 16:24:20 -0700

    Add Glow support and control

--- keyboards/ergodox_ez/rules.mk
+++ keyboards/ergodox_ez/rules.mk
@@ -2,7 +2,17 @@
 # https://geekhack.org/index.php?topic=22780.msg873819#msg873819 for
 # details), include the following define:
 # OPT_DEFS += -DLEFT_LEDS
--
+
+ORYX_ENABLE = yes
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = no   # Enable Bootmagic Lite
+MOUSEKEY_ENABLE  = yes  # Mouse keys
+EXTRAKEY_ENABLE  = yes  # Audio control and System control
+CONSOLE_ENABLE   = no   # Console for debug
+COMMAND_ENABLE   = no   # Commands for debug and configuration
 CUSTOM_MATRIX    = lite # Custom matrix file for the ErgoDox EZ
 NKRO_ENABLE      = yes  # Enable N-Key Rollover
 UNICODE_ENABLE   = no   # Unicode
@@ -11,7 +21,7 @@ SWAP_HANDS_ENABLE= no   # Allow swapping hands of keyboard
 KEY_LOCK_ENABLE = yes   # Enable Key Lock
 CAPS_WORD_ENABLE= yes	# Enable Caps Word
 
-RGB_MATRIX_ENABLE = no # Enable Glow
+RGB_MATRIX_ENABLE = yes # Enable Glow
 RGB_MATRIX_DRIVER = IS31FL3731
 DEBOUNCE_TYPE = sym_eager_pr
 
