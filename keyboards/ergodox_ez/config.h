7e55f0ace7dbe912c3c2db5fa013e66ba21aeecf
sdygert <sethdygert@gmail.com>
2023-04-09 15:00:22 -0700

    Fix build errors upgrading from qmk 0.17.0 to 0.20.5

--- keyboards/ergodox_ez/config.h
+++ keyboards/ergodox_ez/config.h
@@ -20,19 +20,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "config_common.h"
-
 #define ORYX_CONFIGURATOR
 
 // Caps Word
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
-
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0x3297
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    ZSA Technology Labs
-#define PRODUCT_ID      0x4974
-#define PRODUCT         ErgoDox EZ
 
 /* key matrix size */
 #define MATRIX_ROWS 14
@@ -58,6 +49,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MOUSEKEY_WHEEL_INTERVAL     80
 #define MOUSEKEY_WHEEL_MAX_SPEED    4
 #define MOUSEKEY_WHEEL_TIME_TO_MAX  40
+
+#define TAPPING_TOGGLE  1
+
+#define TAPPING_TERM    200
+#define IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
