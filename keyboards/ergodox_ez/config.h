b8a3e3d66693c8583ca8be282ecdb1a90b650873
sdygert <sethdygert@gmail.com>
2023-04-14 20:34:20 -0700

    Revert "Add permissive hold to config.h"
    This reverts commit 8ffbff2701b145fa32bed5d2be6f8e556873cbe6.

--- keyboards/ergodox_ez/config.h
+++ keyboards/ergodox_ez/config.h
@@ -53,7 +53,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TOGGLE  1
 
 #define TAPPING_TERM    200
-#define PERMISSIVE_HOLD // add permissive hold. see https://docs.qmk.fm/#/tap_hold
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
