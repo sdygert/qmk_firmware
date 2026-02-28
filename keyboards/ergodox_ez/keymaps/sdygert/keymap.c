dcc275fbf46fd1d0ddc0615a95407435a8991c20
sdygert <sdygert@users.noreply.github.com>
2026-02-27 20:03:01 -0800

    Update mouse keys to fix breaking changes
    See PR25444

--- keyboards/ergodox_ez/keymaps/sdygert/keymap.c
+++ keyboards/ergodox_ez/keymaps/sdygert/keymap.c
@@ -107,15 +107,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [MOUS] = LAYOUT_ergodox_pretty(
   // left hand
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN3, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, LCTL(KC_C), LCTL(KC_V),   KC_BSPC, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_BTN1, KC_MS_U, KC_BTN2, KC_TRNS, KC_TRNS,     KC_TRNS, KC_MPLY,    KC_ACL0,      KC_ACL1, KC_ACL2, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,                       KC_WH_L,    KC_WH_D,      KC_WH_U, KC_WH_R, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, MS_BTN3, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, LCTL(KC_C), LCTL(KC_V),   KC_BSPC, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, MS_BTN1, MS_UP,   MS_BTN2, KC_TRNS, KC_TRNS,     KC_TRNS, KC_MPLY,    MS_ACL0,      MS_ACL1, MS_ACL2, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, MS_LEFT, MS_DOWN, MS_RGHT, KC_TRNS,                       MS_WHLL,    MS_WHLD,      MS_WHLU, MS_WHLR, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_LCTL, KC_LALT, KC_LSFT, KC_MEH,  KC_TRNS, KC_TRNS,     KC_TRNS, KC_DEL,     KC_MEH,       KC_RSFT, KC_RALT, KC_RCTL, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                            KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 
                                                KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,
                                                         KC_TRNS,     KC_TRNS,
-                                      KC_BTN1, KC_BTN2, KC_TRNS,     KC_TRNS, KC_BTN1,    KC_BTN2
+                                      MS_BTN1, MS_BTN2, KC_TRNS,     KC_TRNS, MS_BTN1,    MS_BTN2
 ),
 /* Keymap 3: Games Layer
  *
