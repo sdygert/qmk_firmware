f87acff82f1475d71fa282b443194b53606dfb46
sdygert <sdygert@users.noreply.github.com>
2026-03-15 12:32:32 -0700

    Add L2 toggle to Enter

--- keyboards/ergodox_ez/keymaps/sdygert/keymap.c
+++ keyboards/ergodox_ez/keymaps/sdygert/keymap.c
@@ -36,8 +36,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        ,-------------.       ,---------------.
  *                                        | Alt  | LGui |       | Alt  |  Esc   |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space|      |------|       |------|  Tab   |Enter |
+ *                                 |      |      | Home |       | PgUp |        |Enter |
+ *                                 | Space|      |------|       |------|  Tab   | / L2 |
  *                                 |      |      | End  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_EQL,   KC_LBRC,        KC_RBRC,        KC_LEFT,        KC_RGHT,                                                        KC_UP,          KC_DOWN,        KC_NO,          KC_CAPS,           KC_PSCR,
                                                                             KC_LALT,        KC_LGUI,              KC_LALT,  KC_ESC,
                                                                                             KC_HOME,              KC_PGUP,
-                                                            KC_SPC,         KC_NO,          KC_END,               KC_PGDN,  KC_TAB,         KC_ENT
+                                                            KC_SPC,         KC_NO,          KC_END,               KC_PGDN,  KC_TAB,         LT(2,KC_ENT)
 ),
 /* Keymap 1: Symbol Layer
  *
