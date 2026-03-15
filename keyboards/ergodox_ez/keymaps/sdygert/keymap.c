88edc541561f15f5b8fe7bd5024812fda54d531f
sdygert <sdygert@users.noreply.github.com>
2026-03-15 12:42:43 -0700

    Fix missing comma
    Comma dropped in https://github.com/sdygert/qmk_firmware/pull/36

--- keyboards/ergodox_ez/keymaps/sdygert/keymap.c
+++ keyboards/ergodox_ez/keymaps/sdygert/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // left hand
   KC_GRV,   LT(0,KC_1),     LT(0,KC_2),     LT(0,KC_3),     LT(0,KC_4),     LT(0,KC_5),     DM_REC1,              KC_NO,    LT(0,KC_6),     LT(0,KC_7),     LT(0,KC_8),     LT(0,KC_9),        LT(0,KC_0),        LT(0,KC_MINS),
   KC_DEL,   KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           DM_PLY1,              TG(1),    KC_Y,           LT(2,KC_U),     KC_I,           KC_O,              KC_P,              LT(0,KC_BSLS),
-  KC_BSPC,  KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,              KC_SCLN            GUI_T(KC_QUOT),
+  KC_BSPC,  KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,              KC_SCLN,            GUI_T(KC_QUOT),
   KC_LSFT,  CTL_T(KC_Z),    ALT_T(KC_X),    SFT_T(KC_C),    MEH_T(KC_V),    KC_B,           KC_NO,                TG(2),    MEH_T(KC_N),    CTL_T(KC_M),    SFT_T(KC_COMM), ALT_T(KC_DOT),     KC_SLSH,           KC_RSFT,
   KC_EQL,   KC_LBRC,        KC_RBRC,        KC_LEFT,        KC_RGHT,                                                        KC_UP,          KC_DOWN,        KC_NO,          KC_CAPS,           KC_PSCR,
                                                                             KC_LALT,        KC_LGUI,              KC_LALT,  KC_ESC,
